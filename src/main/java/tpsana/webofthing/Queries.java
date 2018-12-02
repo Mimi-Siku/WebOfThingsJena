dba5dbe11e7a24e02de628fb1379f69c19f08320
Mimi Siku <melanie.bassette@gmail.com>
2018-12-02 17:32:06 +0100

    Ecrit les requêtes sparql

--- src/main/java/tpsana/webofthing/Queries.java
+++ src/main/java/tpsana/webofthing/Queries.java
@@ -30,51 +30,86 @@ public class Queries {
 
 	private String createObject() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("PREFIX dc:<http://purl.org/dc/elements/1.1/>\n");
+		builder.append("PREFIX sao: <http://iot.ee.surrey.ac.uk/citypulse/resources/ontologies/sao.ttl>\n");
 		builder.append("SELECT ?streamEvent\n");
-		builder.append("WHERE\n");
-		builder.append("{<http://iot.ee.surrey.ac.uk/citypulse/datasets/parking/parkingDataStream> a ?streamEvent}");
+		builder.append("WHERE{\n");
+		builder.append("?streamEvent a sao:streamEvent.");
+		builder.append("}");
+		builder.append("\nLIMIT 20");
 		
 		return builder.toString();
 	}
 	
 	private String createStartEndTime() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("PREFIX dc:<http://purl.org/dc/elements/1.1/>\n");
+		builder.append("PREFIX sao: <http://iot.ee.surrey.ac.uk/citypulse/resources/ontologies/sao.ttl>\n");
+		builder.append("PREFIX tl: <http://purl.org/NET/c4dm/timeline.owl#>\n");
+		builder.append("SELECT ?startTime ?endTime\n");
+		builder.append("WHERE{\n");
+		builder.append("?streamEvent sao:time [tl:end ?endTime; tl:start ?startTime].\n");
+		builder.append("}");
+		builder.append("\nLIMIT 20");
 
 		return builder.toString();
 	}
 	
 	private String createUnitOfMeasurment() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("PREFIX dc:<http://purl.org/dc/elements/1.1/>\n");
+		builder.append("PREFIX sao: <http://iot.ee.surrey.ac.uk/citypulse/resources/ontologies/sao.ttl>\n");
+		builder.append("SELECT ?unitOfMeasurment\n");
+		builder.append("WHERE{\n");
+		builder.append("?subject sao:hasUnitOfMeasurement ?unitOfMeasurment.\n");
+		builder.append("}");
+		builder.append("\nLIMIT 20");
 
 		return builder.toString();
 	}
 	
 	private String createObservationTime() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("PREFIX dc:<http://purl.org/dc/elements/1.1/>\n");
-
+		builder.append("PREFIX sao: <http://iot.ee.surrey.ac.uk/citypulse/resources/ontologies/sao.ttl>\n");
+		builder.append("PREFIX tl: <http://purl.org/NET/c4dm/timeline.owl#>\n");
+		builder.append("SELECT ?observationTime\n");
+		builder.append("WHERE{\n");
+		builder.append("?subject sao:time [tl:at ?observationTime].\n");
+		builder.append("}");
+		builder.append("\nLIMIT 20");
+		
 		return builder.toString();
 	}
 	
 	private String createLongilatitude() {
 		StringBuilder builder = new StringBuilder();
+		builder.append("PREFIX ct: <http://www.insight-centre.org/citytraffic#>\n");
+		builder.append("SELECT ?latitude ?longitude\n");
+		builder.append("WHERE{\n");
+		builder.append("?subject ct:hasFirstNode [ct:hasLatitude ?latitude; ct:hasLongitude ?longitude].\n");
+		builder.append("}");
+		builder.append("\nLIMIT 20");
 
 		return builder.toString();
 	}
 	
 	private String createNodeName() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("PREFIX dc:<http://purl.org/dc/elements/1.1/>\n");
-		
+		builder.append("PREFIX ct: <http://www.insight-centre.org/citytraffic#>\n");
+		builder.append("SELECT ?nodeName\n");
+		builder.append("WHERE{\n");
+		builder.append("?subject ct:hasFirstNode [ct:hasNodeName ?nodeName].\n");
+		builder.append("}");
+		builder.append("\nLIMIT 20");
+
 		return builder.toString();
 	}
 	
 	private String createValues() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("PREFIX dc:<http://purl.org/dc/elements/1.1/>\n");
+		builder.append("PREFIX sao: <http://iot.ee.surrey.ac.uk/citypulse/resources/ontologies/sao.ttl>\n");
+		builder.append("SELECT ?value\n");
+		builder.append("WHERE{\n");
+		builder.append("?subject sao:value ?value.\n");
+		builder.append("}");
+		builder.append("\nLIMIT 20");
 
 		return builder.toString();
 	}
